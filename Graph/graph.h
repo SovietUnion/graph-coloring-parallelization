28f3d9521fd035d33a02eae685c93832e3d2c21e
siu.pak.mok <siu.pak.mok@6ccb0e78-a8e4-9c2a-b123-ff3df386b525>
2012-11-12 04:37:12 +0000

    Starter Files

---
+++ Graph/graph.h
@@ -0,0 +1,24 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+  using namespace std;
+
+  class Graph {
+
+   public:
+
+    // Return true if a and b are neighbours
+    virtual bool isNeighbours(int a, int b) = 0;
+
+    // Return an iterator that will iteratates all the neighbours of a
+    // we could discuss more about this and come to agreement if you find this setup doesn't work
+    virtual iterator neighbours(int a) = 0;
+
+    // Add an edge between a and b
+    virtual void addNeighbour(int a, int b) = 0;
+
+    // Remove an edge between a and b
+    virtual bool removeNeighbour(int a, int b) = 0;
+  }
+
+#ENDIF
