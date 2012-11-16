94f78b52dcd7f76e17980d17abecc541bf3a9293
siu.pak.mok <siu.pak.mok@6ccb0e78-a8e4-9c2a-b123-ff3df386b525>
2012-11-16 05:58:50 +0000

    Graph Checker

---
+++ trunk/Graph_Checker/GraphChecker.h
@@ -0,0 +1,13 @@
+#ifndef GRAPHCHECKER_H
+#define GRAPHCHECKER_H
+#include "../Graph/graph.h"
+
+class GraphChecker {
+
+    public:
+
+      static bool checkGraph(Graph* g, int* c, const int n);
+
+};
+
+#endif
