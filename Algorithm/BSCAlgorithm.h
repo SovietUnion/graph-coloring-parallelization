d65b1995c6a60250e72362e1cbc9dc7ea173bcac
sunyao.xjtu@gmail.com <sunyao.xjtu@gmail.com@6ccb0e78-a8e4-9c2a-b123-ff3df386b525>
2012-11-20 09:17:57 +0000

    BSCAlgorithm.h

---
+++ Algorithm/BSCAlgorithm.h
@@ -0,0 +1,20 @@
+//BSCAlgorithm.h
+#ifndef BSCALGORITHM_H
+#define BSCALGORITHM_H
+#include "Algorithm.h"
+
+class BSCAlgorithm : public Algorithm {
+public:
+
+    // Constructor
+    BSCAlgorithm(Graph* g);
+
+    // Print out the results
+    virtual void printResults();
+
+    // Colour the graph G and return number of colours used
+    virtual int colourGraph();
+};
+
+#endif
+
