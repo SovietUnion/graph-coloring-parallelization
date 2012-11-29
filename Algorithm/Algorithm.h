5ac4b529790c0f0afd718804e4662d13925bf918
sunyao.xjtu@gmail.com <sunyao.xjtu@gmail.com@6ccb0e78-a8e4-9c2a-b123-ff3df386b525>
2012-11-29 10:57:58 +0000

    Similar to MatrixGraph.h

--- Algorithm/Algorithm.h
+++ Algorithm/Algorithm.h
@@ -21,3 +21,34 @@ public:
 };
 
 #endif
+
+/**************************************/
+//For BSC by Yao
+#ifndef ALGORITHM_H
+#define ALGORITHM_H
+
+#include "Graph.h"
+#include "MatrixGraph.h"
+
+using namespace std;
+
+class Algorithm {
+protected:
+    Graph* g_;
+	int *colours;
+	int num_of_nodes;
+
+public:
+	//Constructor
+	Algorithm(int num_nodes, Graph* g);
+	Algorithm(){};
+    // Print out the results
+    virtual void printResults() = 0;
+
+    // Colour the graph G and return number of colours used
+    virtual int colourGraph() = 0;
+	//Destructor
+	Algorithm::~Algorithm();
+};
+
+#endif
