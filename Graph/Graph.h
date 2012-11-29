e4df7c935cf272c3f47b75f12fbd60ab38d63738
siu.pak.mok@gmail.com <siu.pak.mok@gmail.com@6ccb0e78-a8e4-9c2a-b123-ff3df386b525>
2012-11-29 18:15:11 +0000

    Reverted Graph changes

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -1,3 +1,4 @@
+
 #ifndef GRAPH_H
 #define GRAPH_H
 #include <iostream> 
@@ -82,74 +83,6 @@ public:
        g.print(os);
        return os;
     }
-};
-
-#endif
-
-/***********************************************************************/
-//For BSC by Yao
-#ifndef GRAPH_H
-#define GRAPH_H
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-class Graph {
-protected:
-   int * colours_;
-
-public:
-   
-    // Add an edge between a and b
-    virtual void addNeighbour(int a, int b) = 0;
-
-    // Get the vertex degree
-    virtual int getDegree(int vertex) = 0;
-
-    // Get the vertex count in G
-    virtual int getSize() = 0;
-
-    // Set color for a vertex
-    virtual void setColour(int v, int colour) = 0;
-
-    // get colors of graph
-    virtual int* getColours() = 0;
-
-	
-	  //Print out graph
-	  virtual void printGraph()=0;
-
-    //Get all the degrees
-    virtual vector<int> getAllDegree(vector<int> &AllDegree)= 0;
-   
-    //Sort the vertices according to non-increasing degrees
-    virtual vector<int> SortbyDegree(vector<int> &AllDegree)= 0;
-   
-    //Get the set of free colours, which used but not present in neighbor of x
-    virtual void getFreeColours(int x,vector<int> &U)= 0;
-   
-    //Get the number of used colour
-    virtual int getUsedColourNum(int i,vector<int> &U)= 0;
-
-	//Get the larger one of two inputs
-	virtual int getMax(int a, int b)= 0;
-   
-    //Remove colour k
-    virtual void removeColour(int k,vector<int> &U)= 0;
-   
-    //Sort the set of colour(referred as U) according to non-decreasing order
-    virtual void sortU(vector<int> &U)= 0;
-   
-    //Uncolour a vertex
-    virtual void unColour(int x)= 0;
-   
-    //Get the colour of vertex with index x
-    virtual int getColour(int x)= 0;
-    
-
-
 };
 
 #endif
