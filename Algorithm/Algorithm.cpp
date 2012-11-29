8ccee2f3b2b8d4ec64531d9e781e9e2d33ca603f
sunyao.xjtu@gmail.com <sunyao.xjtu@gmail.com@6ccb0e78-a8e4-9c2a-b123-ff3df386b525>
2012-11-29 10:28:58 +0000

    Add my revision

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -21,3 +21,37 @@ void Algorithm::printResults() {
   for (int i = 0; i < num_of_nodes; i++)
     cout << i << " " << colours[i] << endl;
 }
+
+========================================
+I revised this head file like this：
+
+
+#include <iostream>
+#include <stdlib.h>
+#include "Algorithm.h"
+
+using namespace std;
+
+Algorithm::Algorithm (int num_nodes, Graph* g) {
+
+  this->colours = new int[num_nodes];
+  this->num_of_nodes = num_nodes;
+  this->g_= g;
+  for(int i=0;i<num_nodes;i++){
+	  this->colours[i]=0;
+  }
+}
+
+Algorithm::~Algorithm() {
+  delete [] colours;
+}
+
+// Print out the results
+
+void Algorithm::printResults() {
+  for (int i = 0; i < num_of_nodes; i++)
+    cout << i << " " << colours[i] << endl;
+}
+
+--Yao
+=====================================
