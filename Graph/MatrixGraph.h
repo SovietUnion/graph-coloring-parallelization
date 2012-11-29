1154786a9e12d3f4307212ab4a408a26dae6cbf8
sunyao.xjtu@gmail.com <sunyao.xjtu@gmail.com@6ccb0e78-a8e4-9c2a-b123-ff3df386b525>
2012-11-29 10:53:48 +0000

    You may find there are some overlap of my used functions and pre-existing functions, but actually I changed some data type from unsigned int to int for convinence.(I unified all the variable including vertex index, colour index and vertex degree and colour number.etc.to int)

--- Graph/MatrixGraph.h
+++ Graph/MatrixGraph.h
@@ -81,6 +81,76 @@ class MatrixGraph: public Graph {
 
 };
 
+/************************************************************/
+//For BSC by Yao
+#ifndef MATRIXGRAPH_H
+#define MATRIXGRAPH_H
+
+#include "Graph.h"
+using namespace std;
+class MatrixGraph: public Graph {
+
+   public:
+    // Constructor that take in a pregenerated graph
+    MatrixGraph(const vector<vector<bool> > &g, int mSize);
+	
+    // Add an edge between a and b
+    virtual void addNeighbour(int a, int b);
+
+    // Get the vertex degree
+    virtual int getDegree(int vertex);
+   
+    // Get the vertex count in G
+    virtual int getSize();
+
+    // Set color for a vertex
+    virtual void setColour(int v, int colour);
+
+    // get colors of graph
+    virtual int* getColours();
+
+    // format ostream output to printout the graph matrix
+    friend std::ostream& operator<<(ostream &os, const MatrixGraph &g);
+
+	//Print out graph
+	virtual void printGraph();
+   
+/***************************************************************/
+	
+	//For BSC
+
+    //Get all the degrees
+    virtual vector<int> getAllDegree(vector<int> &AllDegree);
+   
+    //Sort the vertices according to non-increasing degrees
+    virtual vector<int> SortbyDegree(vector<int> &AllDegree);
+   
+    //Get the set of free colours, which used but not present in neighbor of x
+    virtual void getFreeColours(int x,vector<int> &U);
+   
+    //Get the number of used colour
+    virtual int getUsedColourNum(int i,vector<int> &U);
+	
+	//Get the larger one of two inputs
+	virtual int getMax(int a, int b);
+   
+    //Remove colour k
+    virtual void removeColour(int k,vector<int> &U);
+   
+    //Sort the set of colour(referred as U) according to non-decreasing order
+    virtual void sortU(vector<int> &U);
+   
+    //Uncolour a vertex
+    virtual void unColour(int x);
+   
+    //Get the colour of vertex with index x
+    virtual int getColour(int x);
+   
+    private:
+        int size;
+        vector<vector<bool> > graph;
+
+};
 
 
 #endif
