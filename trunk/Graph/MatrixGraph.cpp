f5d7c0a453927f4437b5d9cae529902a14a1014e
sunyao.xjtu@gmail.com <sunyao.xjtu@gmail.com@6ccb0e78-a8e4-9c2a-b123-ff3df386b525>
2012-11-29 10:46:52 +0000

    I added some functions used by BSC. Some of those are shared by BSC and Contract. Do I need to write a new pair(with a new name) of .h and .cpp to include those functions for BSC?

--- trunk/Graph/MatrixGraph.cpp
+++ trunk/Graph/MatrixGraph.cpp
@@ -234,3 +234,192 @@ MatrixGraph::print(ostream &os) const
 }
 
 
+/************************************************************************/
+//For BSC by Yao
+#include "MatrixGraph.h"
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+MatrixGraph::MatrixGraph(const vector<vector<bool> > &g, int mSize) {
+    if (mSize <= 0) {
+        throw "Matrix size needs to be greater than 1.\n";
+    }
+    size = mSize;
+    graph = g;
+    colours_ = new int[mSize];
+}
+
+void
+MatrixGraph::addNeighbour(int a, int b) {
+    graph[a][b] = graph[b][a] = 1;
+}
+
+int
+MatrixGraph::getDegree(int vertex) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (graph[vertex][i] == 1) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int
+MatrixGraph::getSize() {
+    return size;
+}
+
+void
+MatrixGraph::setColour(int v, int colour) {
+    colours_[v] = colour;
+}
+
+int *
+MatrixGraph::getColours() {
+
+    return colours_;
+}
+/*
+ostream& operator<<(ostream &os, const MatrixGraph &g) {
+    for (int i = 0; i < g.size; i++) {
+        for (int j = 0; j < g.size; j++) {
+            os << g.graph[i][j] << " ";
+        }
+        os << endl;
+    }
+    return os;
+}*/
+
+void 
+MatrixGraph::printGraph() {
+ for(int i=0;i<getSize();i++){
+		for(int j=0;j<getSize();j++){
+		    cout<<graph[i][j]<<" ";
+		}
+     cout<<endl;
+	}
+}
+
+	//For BSC by Yao
+//Get all the degrees
+
+vector<int> MatrixGraph::getAllDegree(vector<int> &AllDegree){
+	   vector<int> B;
+       for(int i=0;i<getSize();i++){
+       B.push_back(i);
+       AllDegree.push_back(getDegree(i));
+       } 
+	   return B;
+}  
+//Sort the vertices according to non-increasing degrees
+
+vector<int> MatrixGraph::SortbyDegree(vector<int> &AllDegree){
+	       vector<int> B;
+		   B=getAllDegree(AllDegree);
+           int temp;
+		   for(int i=0;i<(getSize()-1);i++){
+           if(AllDegree[B[i]]<AllDegree[B[i+1]]){
+              temp=B[i];
+              B[i]=B[i+1];
+              B[i+1]=temp;
+           }
+       } 
+		   return B;
+}     
+//Get the set of free colours, which used but not present in neighbor of x
+void
+MatrixGraph::getFreeColours(int x,vector<int> &U){
+	        
+           for(int i=0;i<=x;i++){
+			 vector<int> ::iterator it;
+		     for(it=U.begin();it!=U.end();){
+					
+					if(colours_[i]==*it){
+						 it=U.erase(it);
+					}
+                   			
+					else{ 
+						++it;
+					}
+					//Print Current U
+                    vector<int> ::iterator iter;
+					for(iter=U.begin();iter!=U.end();iter++){
+		            cout<<*iter<<" ";
+					}
+					cout<<endl;
+		     }
+		  }         
+			        sort(U.begin(),U.end());
+					/*
+					vector<int> ::iterator iter;
+					for(iter=U.begin();iter!=U.end();iter++){
+		            cout<<*iter<<" "<<endl;
+					}
+					*/
+           
+}
+			
+	
+                  
+//Get the number of used colour
+int 
+MatrixGraph::getUsedColourNum(int i,vector<int> &U){
+     int l,k;
+     if(i>0){
+       l=getUsedColourNum(i-1,U);
+       k=U[0];
+       if(l>k){
+       return l;
+       }
+       else{
+       return k;
+       }
+     }
+	 else {
+	   return 0;
+    }
+}
+int
+MatrixGraph::getMax(int a,int b){
+	if(a>b){
+	  return a;
+	}
+	else{
+	  return b;
+	}
+}
+//Remove colour k
+void
+MatrixGraph::removeColour(int k,vector<int> &U){
+	vector<int> ::iterator it;
+	for(it=U.begin();it!=U.end();){
+					if(k==*it){
+						 it=U.erase(it);
+					}
+                   			
+					else{ 
+						++it;
+					}
+    }
+}
+//Sort the set of colour(U) according to non-decreasing order
+void
+MatrixGraph::sortU(vector<int> &U){
+   sort(U.begin(),U.end());
+}
+//Uncolour a vertex
+void
+MatrixGraph::unColour(int x){
+   colours_[x]=0;
+}
+//Get the colour of vertex with index x
+int 
+MatrixGraph::getColour(int x) {
+	return colours_[x];
+}
+
+
+/***************************************************************/
