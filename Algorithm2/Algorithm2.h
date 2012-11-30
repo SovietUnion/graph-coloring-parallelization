6669bfd19829c84315c92d60d0ac90c61ee4a926
sunyao.xjtu@gmail.com <sunyao.xjtu@gmail.com@6ccb0e78-a8e4-9c2a-b123-ff3df386b525>
2012-11-30 01:53:11 +0000

    no

---
+++ Algorithm2/Algorithm2.h
@@ -0,0 +1,28 @@
+#ifndef ALGORITHM_H
+#define ALGORITHM_H
+
+#include "Graph2.h"
+#include "MatrixGraph2.h"
+
+using namespace std;
+
+class Algorithm {
+protected:
+    Graph* g_;
+	int *colours;
+	int num_of_nodes;
+
+public:
+	//Constructor
+	Algorithm(int num_nodes, Graph* g);
+	Algorithm(){};
+    // Print out the results
+    virtual void printResults() = 0;
+
+    // Colour the graph G and return number of colours used
+    virtual int colourGraph() = 0;
+	//Destructor
+	Algorithm::~Algorithm();
+};
+
+#endif
