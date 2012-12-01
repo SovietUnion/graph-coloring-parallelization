244c5e9e4dbc497c5aacde21bad4ce577be949d6
sunyao.xjtu@gmail.com <sunyao.xjtu@gmail.com@6ccb0e78-a8e4-9c2a-b123-ff3df386b525>
2012-12-01 06:16:09 +0000

    getFreeColours（）is revised

--- trunk/Algorithm2/MatrixGraph2.cpp
+++ trunk/Algorithm2/MatrixGraph2.cpp
@@ -104,24 +104,53 @@ vector<int> MatrixGraph::SortbyDegree(vector<int> &AllDegree){
 }     
 //Get the set of free colours, which used but not present in neighbor of x
 vector<int>
-MatrixGraph::getFreeColours(int x,int i,vector<int> &colors){
+MatrixGraph::getFreeColours(int x){
+	
 	          vector<int> U;
 			  vector<int> Used;
-			 // cout<<"colors[i]+2:"<<colors[i]+2<<endl;
-		   for(int k=1;k<colors[i]+2;k++){
-			   for(int j=0;j<getSize();j++){
+			  int count=0;
+			  for(int k=0;k<getSize();k++){
+				  if(colours_[k]!=0){
+                    Used.push_back(colours_[k]);
+				  }
+			  }
+
+			  sort(Used.begin(),Used.end());
+	          cout<<"Used Colors:"<<endl;
+	          for(int i=0;i<(int)Used.size();i++){
+	              cout<<Used[i]<<" ";
+	          }
+			  cout<<endl;
+
+	          for(int i=0;i<(int)Used.size()-1;i++){
+		        if(Used[i]!=Used[i+1]){
+			            count++;
+		        }
+	          }
+	                count=count+1;
+					cout<<"count:"<<count<<endl;;
+					 Used.clear();
+		     for(int k=1;k<count+2;k++){
+			    for(int j=0;j<getSize();j++){
 				    if(colours_[j]==k && isNeighbours(j,x)){
-						Used.push_back(k);
+						Used.push_back(k);//used in the neighbour of x
 						//cout<<"vertex#"<<j<<" color# "<<k<<endl;
 					    
 					}
 
-		      }
+		       }
               
 		  }
-          for(int k=1;k<colors[i]+2;k++){
-			  U.push_back(k);
+          
+          for(int k=1;k<=count+1;k++){
+			    U.push_back(k);
 		  }
+		  cout<<"Origin U set is:"<<endl;
+	      for(int i=0;i<(int)U.size();i++){
+	           cout<<U[i]<<" ";
+	      }
+	      cout<<endl;
+
 		  vector<int> ::iterator iter;
 		  for(int i=0;i<(int)Used.size();i++){
 			   for(iter=U.begin();iter!=U.end();){
@@ -234,4 +263,5 @@ int
 MatrixGraph::getColour(int x) {
 	return colours_[x];
 }
+
 
