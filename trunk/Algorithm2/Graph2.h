d7bbca0f429d4d4a97f425ff4dd5045c89a745bb
sunyao.xjtu@gmail.com <sunyao.xjtu@gmail.com@6ccb0e78-a8e4-9c2a-b123-ff3df386b525>
2012-12-04 12:45:39 +0000

    Latest code

--- trunk/Algorithm2/Graph2.h
+++ trunk/Algorithm2/Graph2.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -11,7 +12,7 @@ protected:
    int * colours_;
 
 public:
-	  // Return true if a and b are neighbours
+	// Return true if a and b are neighbours
     virtual bool isNeighbours(int a, int b) = 0;
    
     // Add an edge between a and b
@@ -29,10 +30,10 @@ public:
     // get colors of graph
     virtual int* getColours() = 0;
 
-	 //Print out graph
-	 virtual void printGraph()= 0;
+	//Print out graph
+	virtual void printGraph()= 0;
 
-	 // Return an vector containing the neighbours of a
+	// Return an vector containing the neighbours of a
     virtual vector<int> getNeighbours(int a)= 0;
 
     //Get all the degrees
@@ -41,23 +42,32 @@ public:
     //Sort the vertices according to non-increasing degrees
     virtual vector<int> SortbyDegree(vector<int> &AllDegree)= 0;
 
-	  //Get the degree of saturation of vertex x
-	  virtual int getVertexDSATUR(int x)=0;
+	//Maintain a sequence of vertex by non-increasing DSATUR
+    virtual void SortbyDSATUR(vector<int> &B)=0;
 
-	  //Get the vertex with maximum degree of saturation
-	  virtual int getMaxDSATURvertex(vector<int> &A)= 0;
+	//Get the degree of saturation of vertex x
+	virtual int getVertexDSATUR(int x)=0;
+
+	//Get the vertex with maximum degree of saturation
+	virtual int getMaxDSATURvertex(vector<int> &A)= 0;
+
+	//Get colors(i)
+	//virtual int colors(int i)=0;
+
+	//Swap the element in a vector.
+    virtual void swap(int a, int b, vector<int> &A)=0;
    
-     //Get the set of free colours, which used but not present in neighbor of x
-    virtual vector<int> getFreeColours(int x)= 0;
+    //Get the set of free colours, which used but not present in neighbor of x
+    virtual void getFreeColours(int x,set<int> &U)=0;
    
     //Get the number of used colour
     virtual int getUsedColourNum(int i,vector<int> &U)= 0;
 
-	  //Get the larger one of two inputs
-	  virtual int getMax(int a, int b)= 0;
+	//Get the larger one of two inputs
+	virtual int getMax(int a, int b)= 0;
    
     //Remove colour k
-    virtual void removeColour(int k,vector<int> &U)= 0;
+    virtual void removeColour(int k,set<int> &U)= 0;
    
     //Sort the set of colour(referred as U) according to non-decreasing order
     virtual void sortU(vector<int> &U)= 0;
