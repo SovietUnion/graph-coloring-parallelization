4d7dea50e7100409147095fa6b349d7144c49341
sunyao.xjtu@gmail.com <sunyao.xjtu@gmail.com@6ccb0e78-a8e4-9c2a-b123-ff3df386b525>
2012-12-09 06:13:48 +0000

    Merged with my version

--- trunk/Algorithm/Algorithm.cpp
+++ trunk/Algorithm/Algorithm.cpp
@@ -9,7 +9,10 @@ Algorithm::Algorithm (int num_nodes, Graph* g) {
   this->colours = new int[num_nodes];
   this->num_of_nodes = num_nodes;
   this->g = g;
-
+  // Initial the colour to be 0 before colouring 
+  for(int i=0;i<num_nodes;i++){ 
+	  this->colours[i]=0;
+  }
 }
 
 Algorithm::~Algorithm {
