ceb4ac6e65a379f5935a3023aa35fe2f2985071c
siu.pak.mok@gmail.com <siu.pak.mok@gmail.com@6ccb0e78-a8e4-9c2a-b123-ff3df386b525>
2012-12-10 07:24:53 +0000

    BSCAlgorihm

--- Algorithm/BSCAlgorithm.h
+++ Algorithm/BSCAlgorithm.h
@@ -3,18 +3,54 @@
 #define BSCALGORITHM_H
 
 #include "Algorithm.h"
+#include <set>
+#include<queue>
+
 using namespace std;
 
 class BSCAlgorithm : public Algorithm{
 public:
+
+    static const int maxInt     = 2147483647;
+    static const int notInQueue = 2147483646;
+
     // Constructor
     BSCAlgorithm(Graph* g);
 
-    // Print out the results
-    virtual void printResults();
-
     // Colour the graph G and return number of colours used
     virtual int colourGraph();
+
+protected:
+
+    struct SkewHeap {
+      int DSAT;
+      int parent;
+      int left;
+      int right; 
+    };
+
+    struct BSCData {
+
+      // Basic Data
+      int x;
+      int root;
+      int colors;
+
+      set<unsigned int> U;
+      vector<pair<int,int> > undo;
+    };
+
+    // Heap functions
+    static int  mergeHeap(SkewHeap* h, int a, int b);
+    static int  mergeHeap(SkewHeap* h, queue<int>& q);
+    static void popHeap(SkewHeap* h, int root, queue<int>& updates);
+    static void revert(SkewHeap* h, vector<pair<int,int> >& undo, queue<int>& updates);
+
+    // updates 
+    void update(int x, queue<int>& updates, vector<pair<int,int> >& undo,
+                SkewHeap* h, unsigned int* colours);
+
+    void findFreeColour(int a, int colourNumber, set<unsigned int>& neighbour_colours);
 };
 
 #endif
